1eec1aedb1594ee9d2ce7a7a0cfd561f1eb954ed
DANC0D3R <danieleminieri99@gmail.com>
2025-07-10 15:45:19 +0200

    Fix stile

--- script.js
+++ script.js
@@ -534,7 +534,7 @@ function filterMovies(searchTerm) {
     displayMovies(filtered);
 }
 
-window.onload = updateMovieList1;
+window.onload = updateMovieList;
 
 document.querySelector(".search-button").addEventListener("click", function () {
     const searchTerm = document.querySelector(".search-input").value;
